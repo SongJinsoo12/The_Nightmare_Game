4fdf04c621189aa04628632b46b259c45e58ae7d
SongJinsoo12 <jinsoo318@gmail.com>
2026-07-16 11:16:28 +0900

    .

--- The_Nightmare/SkeletonCreators.cs
+++ The_Nightmare/SkeletonCreators.cs
@@ -37,7 +37,7 @@ namespace The_Nightmare
             skeleton.Animator.AddAnimation(MonsterState.PATROL, walkAnim);
 
             Animation dieAnim = new Animation(filePath +
-                "Skeleton_01_White_Die.png", width, height, 13, 0.08, false);
+                "Skeleton_01_White_Die.png", width, height, 13, 0.08, true);
             skeleton.Animator.AddAnimation(MonsterState.DIE, dieAnim);
 
             skeleton.Animator.Play(skeleton.AI.CurState);
