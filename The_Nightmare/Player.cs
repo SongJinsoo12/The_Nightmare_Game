e402e918c3424d15b8327b1817102243bb112914
MENA-dev1 <menatube0721@gmail.com>
2026-07-29 23:19:03 +0900

    공격 박스 위치 수정

--- The_Nightmare/Player.cs
+++ The_Nightmare/Player.cs
@@ -130,10 +130,10 @@ namespace The_Nightmare
 
             switch (FacingDirection)
             {
-                case Direction.Up: targetY -= 1; break;
-                case Direction.Down: targetY += 1; break;
-                case Direction.Left: targetX -= 1; break;
-                case Direction.Right: targetX += 1; break;
+                case Direction.Up: targetY -= 32; break;
+                case Direction.Down: targetY += 32; break;
+                case Direction.Left: targetX -= 32; break;
+                case Direction.Right: targetX += 32; break;
             }
 
             //OnAttacked?.Invoke(targetX, targetY);
